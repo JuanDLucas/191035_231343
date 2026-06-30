2b4b066ed5acae3b3feb158204afa069c7c42b94
Juan D Lucas <juan.rodajau@gmail.com>
2026-06-30 20:48:06 -0300

    finalizando tec em desenvolvimento de sistemas na etec

--- 191035_231343/Views/FrmCaixa.Designer.cs
+++ 191035_231343/Views/FrmCaixa.Designer.cs
@@ -43,7 +43,7 @@
             this.txtPIX = new System.Windows.Forms.TextBox();
             this.txtBoleto = new System.Windows.Forms.TextBox();
             this.txtTroco = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnPagamento = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
@@ -181,18 +181,18 @@
             this.txtTroco.TabIndex = 2;
             this.txtTroco.Text = "R$ 0,00";
             // 
-            // button1
+            // btnPagamento
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
-            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.button1.Location = new System.Drawing.Point(433, 531);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(203, 75);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Realizar Pagamento";
-            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnPagamento.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPagamento.Image = ((System.Drawing.Image)(resources.GetObject("btnPagamento.Image")));
+            this.btnPagamento.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnPagamento.Location = new System.Drawing.Point(433, 531);
+            this.btnPagamento.Name = "btnPagamento";
+            this.btnPagamento.Size = new System.Drawing.Size(203, 75);
+            this.btnPagamento.TabIndex = 3;
+            this.btnPagamento.Text = "Realizar Pagamento";
+            this.btnPagamento.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnPagamento.UseVisualStyleBackColor = true;
             // 
             // pictureBox1
             // 
@@ -254,7 +254,7 @@
             this.Controls.Add(this.pictureBox4);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnPagamento);
             this.Controls.Add(this.txtTroco);
             this.Controls.Add(this.txtBoleto);
             this.Controls.Add(this.txtPIX);
@@ -298,7 +298,7 @@
         private System.Windows.Forms.TextBox txtPIX;
         private System.Windows.Forms.TextBox txtBoleto;
         private System.Windows.Forms.TextBox txtTroco;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnPagamento;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.PictureBox pictureBox4;
